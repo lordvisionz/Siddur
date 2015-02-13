0cf2eb437ecda1a43544806083d10dc9e398c550
Ehud Adler <ehudadler@Ehuds-MBP.home>
2015-02-12 22:52:31 -0500

    Initial Commit

---
+++ Siddur/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Siddur
+//
+//  Created by Ehud Adler on 2/12/15.
+//  Copyright (c) 2015 Ehud Adler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Siddur/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Siddur
+//
+//  Created by Ehud Adler on 2/12/15.
+//  Copyright (c) 2015 Ehud Adler. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
